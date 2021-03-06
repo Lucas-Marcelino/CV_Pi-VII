045c0ff5f999176f613f8be21b56791ea58c67d8
Gustavo Rigor <gusrigor@icloud.com>
2021-03-06 17:00:01 -0300

    Adicionados parametros para a detecção da imagem

--- Documentacao/9.1-bananaDetector.py
+++ Documentacao/9.1-bananaDetector.py
@@ -4,8 +4,12 @@ import numpy as np
 
 frame = cv2.imread('banana.jpg')
 
+kernel = np.ones((5,5),np.uint8)
+
 # Convert BGR to HSV
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+hsv = cv2.medianBlur(hsv,7)
 
 # define range of yellow color in HSV
 lower_yellow = np.array([25,50,50])
@@ -13,6 +17,8 @@ upper_yellow = np.array([32,255,255])
 
 # Threshold the HSV image to get only yellow colors
 mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+mask = cv2.dilate(mask,kernel,iterations = 1)
 
 # Bitwise-AND mask and original image
 res = cv2.bitwise_and(frame,frame, mask= mask)
